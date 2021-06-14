942819ba55edc706b299e5075b5e954368d8a75b
I020461 <bhushan.more@tsb.co.uk>
2021-06-14 13:40:16 +0100

    Initial Project structure

---
+++ src/test/java/com/demo/customrolesengine/CustomRolesEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.customrolesengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomRolesEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
